5d43335b2819babc073aa8a4febed53e68ba8188
Rickcler <117739535+Rickcler@users.noreply.github.com>
2023-03-06 13:06:26 +0100

    Country and Formatting
    Allowing country names from different languages (e.g. Deutschland and Germany for Germany)
    Some Formatting (Adding a frame to the window)

--- WeatherApp.py
+++ WeatherApp.py
@@ -2,8 +2,8 @@
 import requests
 #PySimpleGUI for building the GUI
 import PySimpleGUI as sg
-#PyCountry to convert the Country Name to Iso-Code
-import pycountry
+#Coutntrynames to convert the Country Name to Iso-Code
+import countrynames
 #Base64 for converting the Icon from binary into ASCII
 import base64
 
@@ -13,15 +13,22 @@ api_key = "64cdfea0dbfd192437a08fdf01385a82"
 #Function for changing whole country-name into ISO-Code
 def get_country_code(country_name):
     try:
-        country = pycountry.countries.get(name=country_name)
-        return country.alpha_2
+        country_code = countrynames.to_code(country_name)
+        return country_code
     except AttributeError:
         return None
 # Defining the layout of the initial input value
 layout1 = [
-    [sg.Text('Enter your country:'), sg.InputText(key="country")],
-    [sg.Text('Enter your city:'), sg.InputText(key="city")],
-    [sg.Button('Ok', button_color = ("black", "green"), bind_return_key = True), sg.Button('Cancel', button_color = ("white", "red"))]
+    [sg.Text("Please enter your location data!")],
+    [sg.Frame(
+        layout =[
+        [sg.Text('Country:', size = (20, 1)), sg.InputText(key="country", size = (20, 1))],
+        [sg.Text('City:', size = (20, 1)), sg.InputText(key="city", size = (20, 1))]],
+        title = "Enter here:", relief = sg.RELIEF_GROOVE
+            )
+    ],
+    [sg.Button('Ok', button_color = ("black", "green"), bind_return_key = True), sg.Button('Cancel',button_color= ("white", "red"))]
+
 ]
 # Creating the window
 window1 = sg.Window('Weather App', layout1)
@@ -42,7 +49,7 @@ while True:
 
 #Next step is to get the ISO-Code from the Country input   
 while True:
-    if event == "Cancel":
+    if event in (None, "Cancel"):
         break
     try:
         #If it works the loop will not continue
@@ -53,10 +60,15 @@ while True:
         while True:
             #Creating a new Layout that calls attention to the problem with the country name 
             layout2 =[
-                    [sg.Text("That didn't work! Please try again.")],
-                    [sg.Text("You didn't put in the country name correctly. Please use the official English name")],
-                    [sg.Text('Enter your country:'), sg.InputText(key="country")],
-                    [sg.Text('Enter your city:'), sg.InputText(key="city")],
+                    [sg.Text("That didn't work!")],
+                    [sg.Text("You didn't put in the country name correctly. Please try again.")],
+                    [sg.Frame(
+                              layout =[
+                                       [sg.Text('Country:', size = (20, 1)), sg.InputText(key="country", size = (20, 1))],
+                                       [sg.Text('City:', size = (20, 1)), sg.InputText(key="city", size = (20, 1))]],
+                                       title = "Enter here:", relief = sg.RELIEF_GROOVE
+                             )
+                    ],
                     [sg.Button('Ok', button_color = ("black", "green"), bind_return_key = True), sg.Button('Cancel',button_color= ("white", "red"))]
                     ]
             window2 = sg.Window('Weather App', layout2)
@@ -90,9 +102,15 @@ while True:
         #As we made sure earlier, that the country name is right, it must about the city.
         while True:
             layout2 =[
-                      [sg.Text("That didn't work! Please try again.")],
-                      [sg.Text("Please enter the name of the city in the language of the country.")],
-                      [sg.Text('Enter your city:'), sg.InputText(key="city")],
+                      [sg.Text("That didn't work!")],
+                      [sg.Text("You didn't put the city name correctly. Please try again")]
+                      [sg.Frame(
+                                layout =[
+                                        [sg.Text('Country:', size = (20, 1)), sg.InputText(key="country", size = (20, 1))],
+                                        [sg.Text('City:', size = (20, 1)), sg.InputText(key="city", size = (20, 1))]],
+                                        title = "Enter here:", relief = sg.RELIEF_GROOVE
+                                )
+                      ],
                       [sg.Button('Ok', button_color = ("black", "green"), bind_return_key = True), sg.Button('Cancel', button_color= ("white", "red"))]
                      ]
             window2 = sg.Window('Weather App', layout2)
@@ -174,5 +192,3 @@ while True:
         ]
         window3.close()
         window3 = sg.Window(title = "Results", layout = layout3)
-
-print("PySimpleGUI version:", sg.__version__)
